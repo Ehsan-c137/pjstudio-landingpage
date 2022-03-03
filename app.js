40f2e215ddfa8a7ba0a5d16f32c5c9eecc0e3d42
Ehsan-c137 <ehsanmousavi1.79@gmail.com>
2022-03-03 23:24:54 +0330

    added fade animation on scrolling to Pages

--- app.js
+++ app.js
@@ -8,9 +8,18 @@ const options = {
 
 const callback = (entries, observer) => {
    entries.forEach((entry) => {
-      if (entry.isIntersecting && entry.target.className == "container") {
-         console.log(entry);
+      if (entry.isIntersecting) {
+         entry.target.classList.add("observer-active");
+         console.log(entry.target);
+         // observer.unobserver(entry.target);
       }
    });
 };
 let observer = new IntersectionObserver(callback, options);
+
+const observerEl = document.querySelectorAll(".observer");
+observerEl.forEach((el) => {
+   observer.observe(el);
+});
+
+// observer.observe(document.querySelectorAll(".observer"));
