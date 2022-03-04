a4e4bc3f32a1b769972041882fbc4f8dbc9ae2aa
Ehsan-c137 <ehsanmousavi1.79@gmail.com>
2022-03-04 13:11:54 +0330

    added lazyLoadingImg

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 "use strict";
+// TODO change the width of .phone to 80rem when lazyImg loaded
 
 const options = {
    root: null,
@@ -10,8 +11,8 @@ const callback = (entries, observer) => {
    entries.forEach((entry) => {
       if (entry.isIntersecting) {
          entry.target.classList.add("observer-active");
-         console.log(entry.target);
          // observer.unobserver(entry.target);
+         observer.unobserve(entry.target);
       }
    });
 };
@@ -21,3 +22,29 @@ const observerEl = document.querySelectorAll(".observer");
 observerEl.forEach((el) => {
    observer.observe(el);
 });
+
+// lazy loading img
+const imgTargets = document.querySelectorAll("img[data-src]");
+
+const loadImg = function (entries, observer) {
+   const [entry] = entries;
+
+   if (!entry.isIntersecting) return;
+
+   // replace src with data-src
+   entry.target.src = entry.target.dataset.src;
+
+   console.log(entry.target);
+   entry.target.addEventListener("load", function () {
+      entry.target.classList.remove("lazy-img");
+   });
+   observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+   root: null,
+   threshold: 0,
+   rootMargin: "200px",
+});
+
+imgTargets.forEach((img) => imgObserver.observe(img));
